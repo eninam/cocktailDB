ad0a9ca352ed8cb78f91f08bd3c5f15885b13d15
Eninam <megantako@gmail.com>
2020-08-21 23:57:18 -0400

    added search functionality, and page when a single cocktail is clicked

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useParams} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 export default function SingleCocktail() {
   const {id} = useParams();
   const [loading, setLoading] = React.useState(false)
@@ -15,7 +16,7 @@ export default function SingleCocktail() {
       const {
         strDrink: name, strDrinkThumb: img, 
         strAlcoholic: info,
-        StrCategory: category, strGlass: glass, StrInstructions: instructions,
+        StrCategory: category, strGlass: glass, strInstructions: instructions,
         strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5
       } = data.drinks[0];
       const ingredients = [strIngredient1, strIngredient2,strIngredient3, strIngredient4
@@ -30,19 +31,39 @@ export default function SingleCocktail() {
   } catch(error) {
     
   }
-  setLoading(true);
+  setLoading(false);
 }
   getCocktail()
   }, [id])
 if(loading){
   return <h2 className="section-title">Loading ...</h2>
 } 
-if(!cocktail) {
-  return <h2 className="section-title">no cocktail to display</h2>
+if (!cocktail) {
+  return <h2 className="section-title">no cocktail to display </h2>
+} else {
+  const {name, img, category, info, instructions, ingredients, glass} = cocktail;
+  return <section className="section cocktail-section">
+    <Link to='/' className='btn btn-primary'> back home</Link>
+    <h2 className="section-title">{name}</h2>
+    <div className="drink">
+    <img src={img} alt={name}/>
+    <div className="drink-info">
+      <p>name: {name}</p>
+      <p>category: {category}</p>
+      <p>info: {info}</p>
+      <p>glass: {glass}</p>
+      <p>instructions: {instructions}</p>
+      <p>
+        ingredients : {' '} {
+          ingredients.map((item, index) => {
+            return item? <span key={index}>{item}</span>: null;
+          })
+        }
+      </p>
+    </div>
+    </div>
+  </section>
 }
 
-  return( 
-  <h1>
-  single cocktail page</h1>
-  );
+
 }
